9c7eb0e22b71b5cc0ea7333308ccbf135bd9f9d6
aidien-glitch <aidienkarwa18@gmail.com>
2022-07-27 13:00:23 +0300

    Configure backend connection using proxy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import Drawer from './components/Drawer';
+import React , {Component} from "react";
 
+class App extends Component {
+  state = {
+    armories: []
+  };
 
-export default function App() {
-  return (
-    <>
-      <Drawer/>
-    </>
-  );
+  async componentDidMount() {
+    const response = await fetch('/armories');
+    const body = await response.json();
+    this.setState({armories: body});
+  }
+
+  render() {
+    const {armories} = this.state;
+    
+    return (
+        <div className="App">
+          <header className="App-header">
+           <h1> I am a hero </h1>
+            <div className="App-intro">
+              <h2>Armory</h2>
+              {armories.map(armory =>
+                  <div key={armory.id}>
+                    {armory.weapon_specs} ({armory.weapon_category})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
+export default App;
 
